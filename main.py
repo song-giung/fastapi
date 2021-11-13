34fc8c23ef76c3318de66bb5e4060e75c207cb6c
song-giung <rldnddl87@gmail.com>
2021-11-13 12:45:27 +0900

    query parameter metadata

--- main.py
+++ main.py
@@ -1,66 +1,17 @@
-from enum import Enum
-from typing import Optional, List
+from typing import Optional
+
 from fastapi import FastAPI, Query
-from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put('/items/{item_id}')
-def update_item(item_id: int, item: Item):
-    return {'item_name': item.name, "item_id": item_id}
-
-
-@app.get('/models/{model_name}')
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep LEARNING FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all teh images"}
-
-    return {"model_name": model_name, "message": "have some residuals"}
-
-
-# if path parameter containing paths
-# like .. /files/myhome/work/awesome_python.txt
-@app.get('/files/{file_path:path}')
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-
-fake_items_db = [
-    {"item_name": "foo"},
-    {"item_name": "bar"},
-    {"item_name": "baz"}
-]
-
-
 @app.get('/items/')
-async def read_items(q: list = Query([])):
-    results = {'items': [{'item_id': "Foo"}, {"item_id": "Bar"}]}
+async def read_items(q: Optional[str] = Query(None,
+                                              title="Query string",
+                                              description='Query string for the items',
+                                              min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+
     if q:
         results.update({"q": q})
     return results
